aa5f3a2bf917f1183e4b1720504581d0699a30ef
Duarte Araujo <duartearaujo2002@gmail.com>
2026-03-10 12:43:04 +0000

    ss

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include <src/server/server.hpp>
+#include <includes/server/server.hpp>
 
 /*
 Create a port variable that will read the conf.file. then call the constructor with that port variable,
