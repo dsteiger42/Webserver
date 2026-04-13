eec35eb08a727a42288be152645854f917a726b9
Rafael Matos <rafael.joao.amorim.matos@gmail.com>
2026-04-13 20:04:11 +0100

    implemented sanitize_env

--- src/cgi/cgi_env.cpp
+++ src/cgi/cgi_env.cpp
@@ -6,11 +6,23 @@
 /*   By: rafael <rafael@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/24 01:13:49 by rafael            #+#    #+#             */
-/*   Updated: 2026/04/10 01:28:46 by rafael           ###   ########.fr       */
+/*   Updated: 2026/04/13 20:03:48 by rafael           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <http/cgi/CGI.hpp>
+
+static std::string sanitize_Env(const std::string &value)
+{
+	std::string output;
+	for (size_t i = 0; i < value.size(); i++)
+	{
+		if (value[i] == '\n' || value[i] == '\r' || value[i] == '\0')
+			continue;
+		output += value[i];
+	}
+	return output;
+}
 
 void CGI::build_Environment(const Request &req, const std::string &scriptPath)
 {
@@ -21,16 +33,15 @@ void CGI::build_Environment(const Request &req, const std::string &scriptPath)
 	length = req.get_Body().size();
 	ss << length;
 	std::string contentLength = ss.str();
-	env.push_back("REQUEST_METHOD=" + req.get_Method());
-	env.push_back("QUERY_STRING=" + req.get_Query());
+	env.push_back("REQUEST_METHOD=" + sanitize_Env(req.get_Method()));
+	env.push_back("QUERY_STRING=" + sanitize_Env(req.get_Query()));
 	env.push_back("CONTENT_LENGTH=" + contentLength);
-	env.push_back("CONTENT_TYPE=" + req.get_Header("content-type"));
+	env.push_back("CONTENT_TYPE=" + sanitize_Env(req.get_Header("content-type")));
 	env.push_back("SCRIPT_FILENAME=" + scriptPath);
-	env.push_back("SCRIPT_NAME=" + req.get_Path());
-	env.push_back("SERVER_PROTOCOL=" + req.get_Version());
+	env.push_back("SCRIPT_NAME=" + sanitize_Env (req.get_Path()));
+	env.push_back("SERVER_PROTOCOL=" + sanitize_Env(req.get_Version()));
 	env.push_back("GATEWAY_INTERFACE=CGI/1.1");
 	env.push_back("REDIRECT_STATUS=200");
-
 }
 
 std::vector<char *> CGI::convert_Env(const std::vector<std::string> &env)
