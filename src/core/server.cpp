14612cdc13101a55f1f0522fabe6642719070ded
Rafael Matos <rafael.joao.amorim.matos@gmail.com>
2026-05-04 19:34:01 +0100

    add

--- src/core/server.cpp
+++ src/core/server.cpp
@@ -6,7 +6,7 @@
 /*   By: rafael <rafael@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/24 01:31:55 by rafael            #+#    #+#             */
-/*   Updated: 2026/05/04 18:05:40 by rafael           ###   ########.fr       */
+/*   Updated: 2026/05/04 19:22:45 by rafael           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -208,7 +208,7 @@ SendStatus Server::send_ToClient(std::vector<pollfd> &fds, size_t index)
 void Server::cleanup_TimeoutClients(std::vector<pollfd> &fds,
 	unsigned long tick, int timeoutTicks)
 {
-	const int	INCOMPLETE_REQUEST_TIMEOUT_TICKS = 30;
+	const int	INCOMPLETE_REQUEST_TIMEOUT_TICKS = 100000000;
 	bool		doKill;
 	int			fd;
 
@@ -241,6 +241,7 @@ void Server::cleanup_TimeoutClients(std::vector<pollfd> &fds,
 		{
 			if (tick - client.requestStart > (unsigned long)INCOMPLETE_REQUEST_TIMEOUT_TICKS)
 			{
+				std::cerr << "KKKKKKK\n";
 				std::cout << "Client " << fd << " timed out (incomplete request)\n";
 				client.response = _router.make_ErrorCode(408);
 				std::string raw = client.response.serialize();
